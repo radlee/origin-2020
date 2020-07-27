18a9b4a0945ccd64a3faf6f1349ead851e7b696d
lolito <prof.lolito@gmail.com>
2020-07-27 23:40:26 +0200

    Initial commit

---
+++ precache-manifest.bed2700f4d51fff6e2d6f0432388704c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6092203bdec9cdf2fd612eee416ea4",
+    "url": "/origin/index.html"
+  },
+  {
+    "revision": "018721bdbcec0f99cc54",
+    "url": "/origin/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "29653793b71dcaf57d91",
+    "url": "/origin/static/css/main.b6889062.chunk.css"
+  },
+  {
+    "revision": "018721bdbcec0f99cc54",
+    "url": "/origin/static/js/2.36365ef9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/origin/static/js/2.36365ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29653793b71dcaf57d91",
+    "url": "/origin/static/js/main.77eedc5e.chunk.js"
+  },
+  {
+    "revision": "dd23dab0709f125b7d08",
+    "url": "/origin/static/js/runtime-main.3ea35a32.js"
+  },
+  {
+    "revision": "f0e395d281b7803e660753dee127fef7",
+    "url": "/origin/static/media/bg.f0e395d2.jpg"
+  },
+  {
+    "revision": "2efb6a6dda5449acf2eaaec54d601a98",
+    "url": "/origin/static/media/coding2.2efb6a6d.jpg"
+  },
+  {
+    "revision": "5abea5314e1b6931e68b8b797bbeae7f",
+    "url": "/origin/static/media/logo512.5abea531.png"
+  },
+  {
+    "revision": "9e0378daf4df1a015c9dcd935ba0d314",
+    "url": "/origin/static/media/onepointone.9e0378da.jpg"
+  },
+  {
+    "revision": "aa2f28f45d4f6edbd8d9158ed215191c",
+    "url": "/origin/static/media/origin2020bg.aa2f28f4.jpg"
+  },
+  {
+    "revision": "48d21521abe64445c41f1c6f9bc06a33",
+    "url": "/origin/static/media/phone.48d21521.png"
+  }
+]);
